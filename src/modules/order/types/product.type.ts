6155b81ca8dcfa471317ba133e485acc1f69cd01
Fahad Subzwari Personal <fahadsubzwari924@Fahads-MacBook-Pro.local>
2023-11-23 20:53:10 +0500

    added comment

--- src/modules/order/types/product.type.ts
+++ src/modules/order/types/product.type.ts
@@ -1,6 +1,7 @@
 import { OrderProduct } from 'src/entities/order-product.entity';
 import { Product } from 'src/entities/product.entity';
 
+/* Picking some fields from both Product & OrderProduct to create new type */
 export type OrderProductSubset = Pick<
   OrderProduct,
   'customizeName' | 'price' | 'color' | 'createdAt'

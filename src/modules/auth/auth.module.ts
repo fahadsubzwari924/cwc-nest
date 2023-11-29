8341799839ab8c85e13e8aba897531bc00d420ea
Fahad Subzwari Personal <fahadsubzwari924@Fahads-MacBook-Pro.local>
2023-11-29 21:34:42 +0500

    cwc-23: moved jwt constants from constant file to env file

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,16 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants/constants';
-import { UserService } from '../user/services/user.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: jwtConstants.expiry },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRY') },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
--- src/modules/auth/constants/constants.ts
+++
@@ -1,4 +0,0 @@
-export const jwtConstants = {
-  secret: '&&&c^%4)(**w@!!c%%',
-  expiry: '24h',
-};
